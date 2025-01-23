1505c6bff0f26f91cedb8a60c058659c6ee5bcf8
joaquincodigo <162158281+joaquincodigo@users.noreply.github.com>
2025-01-22 23:56:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,131 @@
-window.addEventListener("load", (event) => {
-  let boardElem = document.getElementById("board");
-  let board = Array.from({ length: 20 }, () => Array(20).fill(0));
+window.addEventListener("load", () => {
+  //  ==================== VARIABLES ====================
+  // let screenHTML = document.getElementById("game-screen");
+  let SNAKE = {
+    body: [
+      { x: 7, y: 10 },
+      { x: 8, y: 10 },
+      { x: 9, y: 10 },
+      { x: 10, y: 10 },
+      { x: 11, y: 10 },
+      { x: 12, y: 10 },
+      { x: 13, y: 10 },
+    ],
 
-	function createRow() {
-		let row = document.createElement("div")
-		row.className = "row"
-		return row
-	}
+    get head() {
+      return this.body[0];
+    },
 
-	function createCell() {
-		let cell = document.createElement("div")
-		cell.className = "cell"
-		return cell
-	}
+    get tail() {
+      return this.body[this.body.length - 1];
+    },
+  };
 
-	function renderBoard() {
-		for (let row = 0; row < board.length; row++) {
-			const rowElem = createRow();
-			boardElem.appendChild(rowElem);
-			
-			for (let cell = 0; cell < board[row].length; cell++) {
-				rowElem.appendChild(createCell());
-			}
-		}
-	}
+  MOVING_DIRECTION = "east";
 
-	// 0 = Empty Space
-	// 1 = Snake
-	// 2 = Food
-	
-	
-	renderBoard()
+  //  ==================== FUNCTIONS ====================
+
+  function mountHtmlGrid() {
+    let gridYSize = 15;
+    let gridXSize = 20;
+
+    for (let i = 0; i <= gridYSize; i++) {
+      // Create a row
+      let row = document.createElement("div");
+      row.classList.add("row");
+
+      for (let j = 0; j <= gridXSize; j++) {
+        // Create a cell
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.setAttribute("id", `${j},${i}`); // Id for editing contents
+
+        // Mount a cell
+        row.appendChild(cell);
+      }
+
+      // Mount a row
+      document.getElementById("game-grid-container").appendChild(row);
+    }
+  }
+
+  function renderFrame() {
+    // Delete current snake body
+    for (const bodyChunk of SNAKE.body) {
+      let oldSnakeCell = document.getElementById(
+        `${bodyChunk.x},${bodyChunk.y}`
+      );
+      oldSnakeCell.classList.remove("snake");
+    }
+    // Update snake body
+    moveSnake(MOVING_DIRECTION);
+
+    // Render new snake body
+    for (const bodyChunk of SNAKE.body) {
+      let newSnakeCell = document.getElementById(
+        `${bodyChunk.x},${bodyChunk.y}`
+      );
+      newSnakeCell.classList.add("snake");
+    }
+  }
+
+  function moveSnake() {
+    // Remove the tail
+    SNAKE.body.pop();
+
+    switch (MOVING_DIRECTION) {
+      // Add the new head
+
+      case "north":
+        SNAKE.body.unshift({ x: SNAKE.head.x, y: SNAKE.head.y - 1 });
+        break;
+
+      case "east":
+        SNAKE.body.unshift({ x: SNAKE.head.x + 1, y: SNAKE.head.y });
+        break;
+
+      case "south":
+        SNAKE.body.unshift({ x: SNAKE.head.x, y: SNAKE.head.y + 1 });
+        break;
+
+      case "west":
+        SNAKE.body.unshift({ x: SNAKE.head.x - 1, y: SNAKE.head.y });
+        break;
+    }
+  }
+
+  function initializeKeyHandling() {
+    document.addEventListener("keydown", (event) => {
+      switch (event.key) {
+        case "ArrowUp":
+          console.log("arrow up!");
+
+          MOVING_DIRECTION = "north";
+          break;
+
+        case "ArrowRight":
+          MOVING_DIRECTION = "east";
+          break;
+
+        case "ArrowDown":
+          MOVING_DIRECTION = "south";
+          break;
+
+        case "ArrowLeft":
+          MOVING_DIRECTION = "west";
+          break;
+      }
+    });
+  }
+
+  function main() {
+    mountHtmlGrid();
+    initializeKeyHandling();
+    setInterval(() => {
+      renderFrame();
+    }, 300);
+  }
+  //  ==================== START ====================
+
+  main();
 });
